19a24bb98131d50d8dc0b5cb9ad23d95dcdd2b45
Vaishnav Puram@DESKTOP-1GKDG5O <puramv307@gmail.com>
2023-10-07 00:13:50 +0530

    added role entity

---
+++ src/main/java/com/exam/examportal/models/Role.java
@@ -0,0 +1,53 @@
+package com.exam.examportal.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "role")
+@Getter
+@Setter
+public class Role {
+    public Role() {
+    }
+
+    public Role(int rid, String role) {
+        this.rid = rid;
+        this.role = role;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int rid;
+    private String role;
+    @OneToMany(cascade = CascadeType.ALL,fetch = FetchType.LAZY,mappedBy = "role")
+    private Set<User_role> userRoles=new HashSet<>();
+
+    public Set<User_role> getUserRoles() {
+        return userRoles;
+    }
+
+    public void setUserRoles(Set<User_role> userRoles) {
+        this.userRoles = userRoles;
+    }
+
+    public void setRid(int rid) {
+        this.rid = rid;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getRid() {
+        return rid;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
