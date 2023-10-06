872a9991b5e5c33f0f25aab53d564157055c7693
Vaishnav Puram@DESKTOP-1GKDG5O <puramv307@gmail.com>
2023-10-06 23:25:11 +0530

    added dependencies and configured DB

---
+++ src/main/java/com/exam/examportal/ExamPortalApplication.java
@@ -0,0 +1,15 @@
+package com.exam.examportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamPortalApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Vaishnav!!");
+
+		SpringApplication.run(ExamPortalApplication.class, args);
+	}
+
+}
