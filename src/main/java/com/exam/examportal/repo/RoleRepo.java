e19c6ea6f8bb97e7838e7b2e1838c88a3debcd6e
Vaishnav Puram@DESKTOP-1GKDG5O <puramv307@gmail.com>
2023-10-09 01:26:13 +0530

    added repo for role

---
+++ src/main/java/com/exam/examportal/repo/RoleRepo.java
@@ -0,0 +1,9 @@
+package com.exam.examportal.repo;
+
+import com.exam.examportal.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepo extends JpaRepository<Role,Integer> {
+}
