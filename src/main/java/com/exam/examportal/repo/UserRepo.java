875b989b618f86a8148b25f0d889b994d13808da
Vaishnav Puram@DESKTOP-1GKDG5O <puramv307@gmail.com>
2023-10-09 01:26:23 +0530

    added repo for user

---
+++ src/main/java/com/exam/examportal/repo/UserRepo.java
@@ -0,0 +1,11 @@
+package com.exam.examportal.repo;
+
+import com.exam.examportal.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User,Integer> {
+    User findByUsername(String name);
+    User findByEmail(String email);
+}
