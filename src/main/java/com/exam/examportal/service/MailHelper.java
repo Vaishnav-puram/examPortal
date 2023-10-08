5923cbf5ece14e05b13dec88e59992185e6812e0
Vaishnav Puram@DESKTOP-1GKDG5O <puramv307@gmail.com>
2023-10-09 01:22:31 +0530

    mail sender helper

---
+++ src/main/java/com/exam/examportal/service/MailHelper.java
@@ -0,0 +1,26 @@
+package com.exam.examportal.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailHelper {
+    @Autowired
+    JavaMailSender javaMailSender;
+    MimeMessage mimeMessage;
+    MimeMessageHelper mimeMessageHelper;
+    public MimeMessage mailSender(String from,String to,String sub,String content) throws MessagingException {
+        mimeMessage=javaMailSender.createMimeMessage();
+        mimeMessageHelper=new MimeMessageHelper(mimeMessage);
+        mimeMessageHelper.setFrom(from);
+        mimeMessageHelper.setSubject(sub);
+        mimeMessageHelper.setTo(to);
+        mimeMessageHelper.setText(content);
+        return mimeMessage;
+
+    }
+}
