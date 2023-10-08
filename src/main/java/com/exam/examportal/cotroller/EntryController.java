14f6a4ca6c9d4ff88f5c0fa56d78e42494b85872
Vaishnav Puram@DESKTOP-1GKDG5O <puramv307@gmail.com>
2023-10-09 01:24:27 +0530

    added controller for creating user ,updating password and sending mail

---
+++ src/main/java/com/exam/examportal/cotroller/EntryController.java
@@ -0,0 +1,57 @@
+package com.exam.examportal.cotroller;
+
+import com.exam.examportal.exceptions.UserAlreadyExists;
+import com.exam.examportal.exceptions.UserNotFound;
+import com.exam.examportal.models.Role;
+import com.exam.examportal.models.User;
+import com.exam.examportal.models.User_role;
+import com.exam.examportal.service.EmailService;
+import com.exam.examportal.service.UserService;
+import jakarta.mail.MessagingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/exam")
+public class EntryController {
+    @Autowired
+    UserService userService;
+    @Autowired
+    EmailService emailService;
+
+
+    @PostMapping("/create")
+    public User createUser(@RequestBody User user) throws UserAlreadyExists {
+
+        Role role=new Role();
+        role.setRid(124);
+        role.setRole("ADMIN");
+        User_role userRole=new User_role();
+        userRole.setUser(user);
+        userRole.setRole(role);
+        Set<User_role> user_roles=new HashSet<>();
+        user_roles.add(userRole);
+        return userService.createUser(user,user_roles);
+    }
+
+    @GetMapping("/sendMail")
+    public String sendMail(@RequestParam String TO,@RequestParam String msg) throws MessagingException {
+            emailService.sendMail(TO,msg);
+
+        return "Mail sent successfully";
+    }
+    @GetMapping("/sendOTP")
+    public String sendOTP(@RequestParam String to)throws MessagingException{
+        emailService.sendOTP(to);
+        return "OTP sent successfully";
+    }
+    @GetMapping("/forgetPassword")
+    public String forgetPassword(@RequestParam String email,@RequestParam String password) throws UserNotFound {
+        return userService.updatePassword(email,password);
+    }
+}
