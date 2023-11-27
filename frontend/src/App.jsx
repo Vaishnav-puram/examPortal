40eab17eba9764bb0c7bc2d2461c2e0e68afc795
Vaishnav Puram <puramv307@gmail.com>
2023-11-27 22:38:51 +0530

    added component ToastContainer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
     <>
      <BrowserRouter>
+     <ToastContainer/>
       <Routes>
         <Route path='/signin' element={<SignIn/>}/>
         <Route path='/signup' element={<SignUp/>}/>
