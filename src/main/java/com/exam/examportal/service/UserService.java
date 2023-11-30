4e3ab68cedbe3d86d65fdd76fb18510b9503e233
Vaishnav Puram <puramv307@gmail.com>
2023-11-30 22:20:29 +0530

    added services

--- src/main/java/com/exam/examportal/service/UserService.java
+++ src/main/java/com/exam/examportal/service/UserService.java
@@ -1,9 +1,14 @@
 package com.exam.examportal.service;
 
+import com.exam.examportal.exceptions.FacultyAlreadyExists;
+import com.exam.examportal.exceptions.FacultyNotFound;
 import com.exam.examportal.exceptions.UserAlreadyExists;
 import com.exam.examportal.exceptions.UserNotFound;
+import com.exam.examportal.models.Faculty;
+import com.exam.examportal.models.Role;
 import com.exam.examportal.models.User;
 import com.exam.examportal.models.User_role;
+import com.exam.examportal.repo.FacultyRepo;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
@@ -12,4 +17,14 @@ import java.util.Set;
 public interface UserService {
     public User createUser(User user, Set<User_role> userRoles) throws UserAlreadyExists;
     public String updatePassword(String email,String password)throws UserNotFound;
+
+    User getUser(String rollno);
+    Role getRole(int rid);
+
+    String authenticateCred(String rollno, String password);
+
+    Faculty createFaculty(Faculty faculty) throws FacultyAlreadyExists;
+    Faculty getFaculty(String email) throws FacultyNotFound;
+
+    boolean authenticateCredFaculty(String email, String password);
 }
