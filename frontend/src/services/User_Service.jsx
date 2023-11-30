045f76791a6d051b8f9104def970949564b1ae6a
Vaishnav Puram <puramv307@gmail.com>
2023-11-30 22:19:29 +0530

    added signin axios helper

--- frontend/src/services/User_Service.jsx
+++ frontend/src/services/User_Service.jsx
@@ -4,3 +4,12 @@ export const signUp=(user)=>{
     return axiosService.post('/create',user)
             .then((response)=>{response.data})
 }
+export const signin=(user)=>{
+    const{rollno,password}=user;
+    const loginURL=`/login/${rollno}?password=${password}`
+    return axiosService.get(loginURL,user)
+        .then((response)=>{
+            console.log(response.data)
+            return response.data
+        })
+}
