899bcfa93591346fdeba5b7af6fa536bb0b04e54
Vaishnav Puram <puramv307@gmail.com>
2023-12-02 00:12:58 +0530

    implementation of UserDetailsService

---
+++ src/main/java/com/exam/examportal/service/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.exam.examportal.service;
+
+import com.exam.examportal.exceptions.UserNotFound;
+import com.exam.examportal.models.User;
+import com.exam.examportal.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepo userRepo;
+    @Override
+    public UserDetails loadUserByUsername(String rollno) throws UsernameNotFoundException {
+        User user=userRepo.findByRollno(rollno);
+        if(user==null){
+            throw new UsernameNotFoundException("No user found!");
+        }
+        return user;
+    }
+}
