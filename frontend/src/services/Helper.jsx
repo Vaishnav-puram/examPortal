5a6485a06243b395fb9d9140493f2f7b1d86d3ea
Vaishnav Puram <puramv307@gmail.com>
2023-12-04 23:47:14 +0530

    changed port

--- frontend/src/services/Helper.jsx
+++ frontend/src/services/Helper.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const BASE_URL='http://localhost:9192/exam';
+export const BASE_URL='http://localhost:9195/exam';
 
 export const axiosService=axios.create({
     baseURL:BASE_URL
