b435d7d8fab5d40a2247cc9608d8719a9275c434
Vaishnav Puram <puramv307@gmail.com>
2023-12-04 23:50:08 +0530

    added response headers

--- src/main/java/com/exam/examportal/security/JWTAuthenticationFilter.java
+++ src/main/java/com/exam/examportal/security/JWTAuthenticationFilter.java
@@ -33,12 +33,13 @@ public class JWTAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-//        try {
-//            Thread.sleep(500);
-//        } catch (InterruptedException e) {
-//            throw new RuntimeException(e);
-//        }
         //Authorization
+        response.setHeader("Access-Control-Allow-Origin", "*"); // * = all domainName
+        response.setHeader("Access-Control-Allow-Credentials", "true"); // allow CrossDomain to use Origin Domain
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+        response.setHeader("Access-Control-Max-Age", "3600"); // Preflight cache duration in browser
+//        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+        response.setHeader("Access-Control-Allow-Headers", "*"); // all header
 
         final String requestHeader = request.getHeader("Authorization");
         //Bearer 2352345235sdfrsfgsdfsdf
