0f6ce3ddf6c6c485f9fefa3df4d7bedaa4861b49
Vaishnav Puram <puramv307@gmail.com>
2023-12-04 23:45:26 +0530

    added routes to admin and user dashboards

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
+import AdminDashBoard from './components/admin/adminDashBoard';
+import UserDashBoard from './components/user/userDashBoard';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Home from './components/Home';
+
 function App() {
+
  
   return (
     <>
@@ -15,6 +19,8 @@ function App() {
         <Route path='/' element={<Home/>}/>
         <Route path='/signin' element={<SignIn/>}/>
         <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/admin-dashboard' element={<AdminDashBoard/>}/>
+        <Route path='/user-dashboard' element={<UserDashBoard/>}/>
       </Routes>
      </BrowserRouter>
     </>
