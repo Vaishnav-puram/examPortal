e0eb3820c56533d935da529d215fee852b811c67
Vaishnav Puram <puramv307@gmail.com>
2023-12-04 23:49:07 +0530

    refactored

--- src/main/java/com/exam/examportal/config/SecurityConfig.java
+++ src/main/java/com/exam/examportal/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.exam.examportal.security.JWTAuthenticationEntryPoint;
 import com.exam.examportal.security.JWTAuthenticationFilter;
 import com.exam.examportal.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
@@ -33,9 +34,16 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableMethodSecurity
+@CrossOrigin("*")
 public class SecurityConfig {
 
     @Autowired
@@ -49,7 +57,7 @@ public class SecurityConfig {
     }
 
     public UserDetailsService userDetailsService() {
-        UserDetails adminUser = User.withUsername("239110").password(passwordEncoder().encode("password@123")).roles("ADMIN").build();
+        UserDetails adminUser = User.withUsername("239110").password(passwordEncoder().encode("password123")).roles("ADMIN").build();
         InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager(adminUser);
         return inMemoryUserDetailsManager;
     }
@@ -58,11 +66,10 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.csrf(csrf -> csrf.disable())
-                .cors(cors->cors.disable())
-                .authorizeHttpRequests(auth->auth.requestMatchers("/home/**")
-                        .authenticated().requestMatchers("/exam/generate-token").permitAll().anyRequest()
-                        .authenticated())
-                .exceptionHandling(ex -> ex.authenticationEntryPoint(point))
+                .authorizeRequests()
+                .requestMatchers("/exam/*").permitAll().anyRequest()
+                        .authenticated()
+                .and()
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
