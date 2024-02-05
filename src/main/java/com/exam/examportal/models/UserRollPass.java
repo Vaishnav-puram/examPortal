2b018865a85c0f6dd7c7f348677fbe772f5ab4b8
Vaishnav Puram <puramv307@gmail.com>
2024-02-06 00:02:14 +0530

    User-Rollno-NewPassword POJO

---
+++ src/main/java/com/exam/examportal/models/UserRollPass.java
@@ -0,0 +1,22 @@
+package com.exam.examportal.models;
+
+public class UserRollPass {
+    private String rollno;
+    private String newPass;
+
+    public String getRollno() {
+        return rollno;
+    }
+
+    public void setRollno(String rollno) {
+        this.rollno = rollno;
+    }
+
+    public String getNewPass() {
+        return newPass;
+    }
+
+    public void setNewPass(String newPass) {
+        this.newPass = newPass;
+    }
+}
