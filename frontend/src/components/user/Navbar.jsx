ae10cd86e079f3832791a2ec09c1f42e9404fcc1
Vaishnav Puram <puramv307@gmail.com>
2024-02-05 23:57:45 +0530

    updated navbar

--- frontend/src/components/user/Navbar.jsx
+++ frontend/src/components/user/Navbar.jsx
@@ -1,6 +1,13 @@
-import { NavLink } from "react-router-dom";
+import { NavLink} from "react-router-dom";
 import { getRole,logoff,getUserName} from "../../services/User_Service";
+import { useState } from "react";
+import Sidebar from "./Sidebar";
+import "../../index.css"
 function Navbar(){
+    const [isSidebarOpen,setIsSidebarOpen]=useState(false);
+    const toogleSidebar=()=>{
+        setIsSidebarOpen(!isSidebarOpen);
+    }
     let role=getRole();
     const handleLogout=()=>{
         logoff(); //clears the token, userdetails from local storage
@@ -10,9 +17,12 @@ function Navbar(){
     return(
         <>
              <ul>
-                <span id="registered" style={{float:'right', marginRight:'30px'}} to={'/signup'}><span style={{fontSize:'small'}}>Logged in as</span> {role} {uname}</span>
-                <NavLink id="loginBtn" style={{float:'right', marginRight:'20px'}} to={'/signin'} onClick={handleLogout}>Logout</NavLink>
+                <span id="menu" style={{float:'left', marginRight:'30px'}} onClick={toogleSidebar}>Menu</span>
+                {/* <NavLink to={'/admin-dashboard/menu'} id="menu">Menu</NavLink> */}
+                <span id="registered" style={{float:'right', marginRight:'30px'}} to={'/signup'}>{uname}</span>
+                <NavLink id="loginBtn" style={{float:'right', marginRight:'20px'}} to={'/signin'} onClick={handleLogout}>Logout</NavLink>   
             </ul>
+            {isSidebarOpen && <Sidebar/>}
         </>
     )
 }
