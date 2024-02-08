28ccaaacfbbac0ccce0fbf6733cb091a95fe29cb
Vaishnav Puram <puramv307@gmail.com>
2024-02-08 22:00:47 +0530

    faculty navbar component

---
+++ frontend/src/components/faculty/Navbar.jsx
@@ -0,0 +1,29 @@
+import { NavLink} from "react-router-dom";
+import { getRole,logoff,currFacultyName} from "../../services/User_Service";
+import { useState } from "react";
+// import Sidebar from "./Sidebar";
+import "../../index.css"
+function Navbar(){
+    const [isSidebarOpen,setIsSidebarOpen]=useState(false);
+    const toogleSidebar=()=>{
+        setIsSidebarOpen(!isSidebarOpen);
+    }
+    let role=getRole();
+    const handleLogout=()=>{
+        logoff(); //clears the token, userdetails from local storage
+    }
+    let fname=currFacultyName();
+    console.log(role);
+    return(
+        <>
+             <ul>
+                <span id="menu" style={{float:'left', marginRight:'30px'}} onClick={toogleSidebar}>Menu</span>
+                {/* <NavLink to={'/admin-dashboard/menu'} id="menu">Menu</NavLink> */}
+                <span id="registered" style={{float:'right', marginRight:'30px'}} to={'/signup'}>{fname.toUpperCase()}</span>
+                <NavLink id="loginBtn" style={{float:'right', marginRight:'20px'}} to={'/'} onClick={handleLogout}>Logout</NavLink>   
+            </ul>
+            {/* {isSidebarOpen && <Sidebar/>} */}
+        </>
+    )
+}
+export default Navbar;
