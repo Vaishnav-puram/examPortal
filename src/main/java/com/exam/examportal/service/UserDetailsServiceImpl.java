fd650969fec6510f3e846e17787886546d9edd9a
Vaishnav Puram <puramv307@gmail.com>
2024-02-08 21:52:43 +0530

    annotation with qualifier

--- src/main/java/com/exam/examportal/service/UserDetailsServiceImpl.java
+++ src/main/java/com/exam/examportal/service/UserDetailsServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-@Service
+@Service("userDetailsServiceImpl")
 public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     UserRepo userRepo;
