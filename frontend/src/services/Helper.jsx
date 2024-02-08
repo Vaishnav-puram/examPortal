ad7478c2f3ba52fc7e3f93f2245e06eba82bf876
Vaishnav Puram <puramv307@gmail.com>
2024-02-08 21:57:15 +0530

    faculty endpoint

--- frontend/src/services/Helper.jsx
+++ frontend/src/services/Helper.jsx
@@ -1,6 +1,11 @@
 import axios from "axios";
 export const BASE_URL='http://localhost:9195/exam';
+export const Faculty_BASE_URL='http://localhost:9195/faculty';
 
 export const axiosService=axios.create({
     baseURL:BASE_URL
+})
+
+export const axiosFacultyService=axios.create({
+    baseURL:Faculty_BASE_URL
 })
