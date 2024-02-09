1d2b26ac417c135f3d8d418c8885ac12046fee94
Vaishnav Puram <puramv307@gmail.com>
2024-02-10 02:19:46 +0530

    added endpoints - image handling

--- src/main/java/com/exam/examportal/cotroller/EntryController.java
+++ src/main/java/com/exam/examportal/cotroller/EntryController.java
@@ -5,19 +5,26 @@ import com.exam.examportal.exceptions.FacultyNotFound;
 import com.exam.examportal.exceptions.UserAlreadyExists;
 import com.exam.examportal.exceptions.UserNotFound;
 import com.exam.examportal.models.*;
-import com.exam.examportal.service.EmailService;
-import com.exam.examportal.service.UserOTPService;
-import com.exam.examportal.service.UserService;
+import com.exam.examportal.models.QuizModel.Quiz;
+import com.exam.examportal.service.*;
 import jakarta.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
+import static org.springframework.http.MediaType.IMAGE_GIF_VALUE;
+import static org.springframework.http.MediaType.IMAGE_JPEG_VALUE;
+import static org.springframework.http.MediaType.IMAGE_PNG_VALUE;
 
 @RestController
 @RequestMapping("/exam")
@@ -29,6 +36,11 @@ public class EntryController {
     EmailService emailService;
     @Autowired
     UserOTPService userOTPService;
+    @Autowired
+    QuizService quizService;
+
+    @Autowired
+    private ImageHandlingService imageService;
 
     @Autowired
     BCryptPasswordEncoder passwordEncoder;
@@ -81,5 +93,31 @@ public class EntryController {
     @PostMapping("/forgetPassword")
     public String forgetPassword(@RequestBody UserRollPass userRollPass) throws UserNotFound {
         return userService.updatePassword(userRollPass.getRollno(),userRollPass.getNewPass());
+    }
+
+    @PostMapping(value = "/createUserAndImage", consumes ={ MediaType.MULTIPART_FORM_DATA_VALUE,IMAGE_GIF_VALUE, IMAGE_JPEG_VALUE, IMAGE_PNG_VALUE,MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<?> createUserAndImage(@RequestPart MultipartFile image, @RequestPart User user)
+            throws IOException, UserAlreadyExists {
+        System.out.println("in upload user details n image " + user + " " + image);
+        createUser(user);
+        uploadImage(user.getRollno(),image);
+        return ResponseEntity.ok("Created successfully");
+    }
+    @PostMapping(value = "/images/{rollno}", consumes ={ MediaType.MULTIPART_FORM_DATA_VALUE,IMAGE_GIF_VALUE, IMAGE_JPEG_VALUE, IMAGE_PNG_VALUE})
+    public ResponseEntity<?> uploadImage(@PathVariable String rollno, @RequestParam MultipartFile image)
+            throws IOException {
+        System.out.println("in upload image " + rollno);
+        System.out.println("image --> "+image);
+        return ResponseEntity.status(HttpStatus.CREATED).body(imageService.uploadImage(rollno, image));
+    }
+    @GetMapping("/getImage/{rollno}")
+    public byte[] getImageForUser(@PathVariable String rollno) throws IOException {
+        return imageService.serveImage(rollno);
+    }
+
+    @GetMapping("/getQuizBySubject/{subject}") //to include special characters and to consume url as plain text without URL decoding
+    public ResponseEntity<Set<Quiz>> getQuizBySubject(@PathVariable String subject){
+        System.out.println("inside getQuizBySubject"+subject);
+        return ResponseEntity.ok(quizService.findQuizBySubject(subject));
     }
 }
