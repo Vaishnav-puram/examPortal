c2dc644cd73d182e5935f435a05d7240953d950f
Vaishnav Puram <puramv307@gmail.com>
2024-02-10 02:20:28 +0530

    updated sidebar

--- frontend/src/components/admin/Sidebar.jsx
+++ frontend/src/components/admin/Sidebar.jsx
@@ -5,11 +5,14 @@ import {
     CDBSidebarMenuItem,
 } from 'cdbreact';
 import { NavLink} from "react-router-dom";
+import { BiCategory } from "react-icons/bi";
+import { BsBoxes } from "react-icons/bs";
+import { BsBookHalf } from "react-icons/bs";
 function Sidebar() {
     return (
         <>
-            <div style={{height: '100vh',position:'absolute'}}>
-                <CDBSidebar textColor="#fff" backgroundColor="#333">
+            <div style={{height: '90vh',position:'absolute'}}>
+                <CDBSidebar maxWidth='210px' textColor="#fff" backgroundColor="#333">
                     <CDBSidebarContent >
                         <CDBSidebarMenu>
                             <NavLink to={'/admin-dashboard/admin-home'} activeClassName="activeClicked">
@@ -17,20 +20,15 @@ function Sidebar() {
                             </NavLink>
                             <NavLink to={'/admin-dashboard/profile'} ClassName="activeClicked">
                                 <CDBSidebarMenuItem icon="user">Profile</CDBSidebarMenuItem>
-                            </NavLink>
-                            <NavLink exact to="/tables" activeClassName="activeClicked">
-                                <CDBSidebarMenuItem icon="gear"><i className="bi bi-gear-wide-connected"></i>Category</CDBSidebarMenuItem>
-                            </NavLink>
-                            
+                            </NavLink> 
                             <NavLink to={'/admin-dashboard/categories'} activeClassName="activeClicked">
-                                <CDBSidebarMenuItem icon="chart-line">Add Category</CDBSidebarMenuItem>
+                                <CDBSidebarMenuItem><BiCategory /> Categories</CDBSidebarMenuItem>
                             </NavLink>
-
-                            <NavLink exact to="/hero404" target="_blank" activeClassName="activeClicked">
-                                <CDBSidebarMenuItem icon="exclamation-circle">Quizzes</CDBSidebarMenuItem>
+                            <NavLink to={'/admin-dashboard/categoriesForQuiz'} activeClassName="activeClicked">
+                                <CDBSidebarMenuItem><BsBookHalf /> Add Quiz</CDBSidebarMenuItem>
                             </NavLink>
-                            <NavLink exact to="/hero404" target="_blank" activeClassName="activeClicked">
-                                <CDBSidebarMenuItem icon="exclamation-circle">Add Quiz</CDBSidebarMenuItem>
+                            <NavLink to={'/admin-dashboard/getQuizzes'} activeClassName="activeClicked">
+                                <CDBSidebarMenuItem><BsBoxes /> Quizzes</CDBSidebarMenuItem>
                             </NavLink>
                         </CDBSidebarMenu>
                         
--- frontend/src/components/user/Sidebar.jsx
+++ frontend/src/components/user/Sidebar.jsx
@@ -5,30 +5,41 @@ import {
     CDBSidebarMenuItem,
 } from 'cdbreact';
 import { NavLink} from "react-router-dom";
+import { useState,useEffect } from 'react';
+import { getCategories } from '../../services/User_Service';
 function Sidebar() {
+    let [categories,setCategories]=useState([]);
+    useEffect(()=>{
+        fetchCategories();
+    },[])
+
+    const fetchCategories=async ()=>{
+           try{
+                const res=await getCategories();
+                console.log(res.data);
+                setCategories(res.data);
+           }catch(err){
+            console.log(err);
+           }
+    }
     return (
         <>
-            <div style={{height: '100vh',position:'absolute'}}>
-                <CDBSidebar textColor="#fff" backgroundColor="#333">
-                    <CDBSidebarContent >
+            <div style={{height: '90vh',position:'absolute'}}>
+                <CDBSidebar maxWidth='345px' textColor="#fff" backgroundColor="#333">
+                    <CDBSidebarContent>
                         <CDBSidebarMenu>
                             <NavLink to={'/user-dashboard/user-home'} activeClassName="activeClicked">
                                 <CDBSidebarMenuItem icon="home">Home</CDBSidebarMenuItem>
                             </NavLink>
-                            <NavLink to={'/user-dashboard/profile'} ClassName="activeClicked">
+                            <NavLink to={'/user-dashboard/profile'} activeClassName="activeClicked">
                                 <CDBSidebarMenuItem icon="user">Profile</CDBSidebarMenuItem>
                             </NavLink>
-                            <NavLink exact to="/tables" activeClassName="activeClicked">
-                                <CDBSidebarMenuItem icon="table">Tables</CDBSidebarMenuItem>
-                            </NavLink>
+                            {categories.map((category)=>(
+                                <NavLink to={`/user-dashboard/quizzes/${category.title}`} key={category.cid} activeClassName="activeClicked">
+                                <CDBSidebarMenuItem>{category.title}</CDBSidebarMenuItem>
+                                </NavLink>
+                            ))}
                             
-                            <NavLink exact to="/analytics" activeClassName="activeClicked">
-                                <CDBSidebarMenuItem icon="chart-line">Analytics</CDBSidebarMenuItem>
-                            </NavLink>
-
-                            <NavLink exact to="/hero404" target="_blank" activeClassName="activeClicked">
-                                <CDBSidebarMenuItem icon="exclamation-circle">404 page</CDBSidebarMenuItem>
-                            </NavLink>
                         </CDBSidebarMenu>
                         
                     </CDBSidebarContent>
