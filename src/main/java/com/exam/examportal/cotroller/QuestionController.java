30c4674d46a3f57b97e125e3824d7959ab559547
Vaishnav Puram <puramv307@gmail.com>
2024-02-10 19:33:41 +0530

    Question related DTO

--- src/main/java/com/exam/examportal/cotroller/QuestionController.java
+++ src/main/java/com/exam/examportal/cotroller/QuestionController.java
@@ -1,11 +1,13 @@
 package com.exam.examportal.cotroller;
 
+import com.exam.examportal.dto.QuestionDTO;
 import com.exam.examportal.models.QuizModel.Question;
 import com.exam.examportal.models.QuizModel.Quiz;
 import com.exam.examportal.repo.QuizRepo;
 import com.exam.examportal.service.QuestionService;
 import com.exam.examportal.service.QuizService;
 import org.hibernate.sql.ast.tree.expression.Collation;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +15,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/exam/question")
@@ -22,12 +25,14 @@ public class QuestionController {
     private QuestionService questionService;
     @Autowired
     private  QuizService quizService;
+    @Autowired
+    private ModelMapper modelMapper;
     @GetMapping("/getQuestion/{queId}")
     public Question getQuestion(@PathVariable Long queId){
         return questionService.getByQueId(queId);
     }
     @GetMapping("/getQuestionsByQuiz/{qid}")
-    public List<Question> getQuestionsByQuiz(@PathVariable Long qid){
+    public List<QuestionDTO> getQuestionsByQuiz(@PathVariable Long qid){
         Quiz quiz=quizService.getQuizById(qid);
         Set<Question> questionSet=quiz.getQuestionSet();
         List<Question> questionList=new ArrayList<>(questionSet);
@@ -35,7 +40,7 @@ public class QuestionController {
             questionList=questionList.subList(0,quiz.getNoOfQuestions()+1);
         }
         Collections.shuffle(questionList);
-        return questionList;
+        return questionList.stream().map(question ->modelMapper.map(question,QuestionDTO.class)).collect(Collectors.toList());
     }
     @PostMapping("/addQuestion")
     public Question addQuestion(@RequestBody Question question){
---
+++ src/main/java/com/exam/examportal/dto/QuestionDTO.java
@@ -0,0 +1,84 @@
+package com.exam.examportal.dto;
+
+import jakarta.persistence.Column;
+
+public class QuestionDTO {
+    private Long queId;
+
+    @Column(length = 5000)
+    private String content;
+    private String image;
+    private String option_1;
+    private String option_2;
+    private String option_3;
+    private String option_4;
+
+    public QuestionDTO() {
+    }
+
+    public QuestionDTO(Long queId, String content, String image, String option_1, String option_2, String option_3, String option_4) {
+        this.queId = queId;
+        this.content = content;
+        this.image = image;
+        this.option_1 = option_1;
+        this.option_2 = option_2;
+        this.option_3 = option_3;
+        this.option_4 = option_4;
+    }
+
+    public Long getQueId() {
+        return queId;
+    }
+
+    public void setQueId(Long queId) {
+        this.queId = queId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getOption_1() {
+        return option_1;
+    }
+
+    public void setOption_1(String option_1) {
+        this.option_1 = option_1;
+    }
+
+    public String getOption_2() {
+        return option_2;
+    }
+
+    public void setOption_2(String option_2) {
+        this.option_2 = option_2;
+    }
+
+    public String getOption_3() {
+        return option_3;
+    }
+
+    public void setOption_3(String option_3) {
+        this.option_3 = option_3;
+    }
+
+    public String getOption_4() {
+        return option_4;
+    }
+
+    public void setOption_4(String option_4) {
+        this.option_4 = option_4;
+    }
+}
