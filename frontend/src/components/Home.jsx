4f6e9857522f1b73bf5081a92db82b02c6e4488f
Vaishnav Puram <puramv307@gmail.com>
2024-02-10 23:08:46 +0530

    refactored

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,6 +1,5 @@
 import Header from "./Header";
 import { NavLink } from "react-router-dom";
-import FacultySignIn from "./faculty/FacultySignIn";
 import "../index.css"
 import { IoHome } from "react-icons/io5";
 import { FiLogIn } from "react-icons/fi";
@@ -15,9 +14,8 @@ function Home() {
                 <li>About</li>
                 <li>Contact</li>
                 <NavLink id="signupBtn" style={{float:'right', marginRight:'30px'}} to={'/signup'}>SignUp <GoPersonAdd style={{marginBottom:'5px',marginLeft:'2px'}}/></NavLink>
-                <NavLink id="loginBtn" style={{float:'right', marginRight:'20px'}} to={'/signin'}>Login <FiLogIn style={{marginBottom:'5px',marginLeft:'2px'}}/></NavLink>
+                <NavLink id="loginBtn" style={{float:'right', marginRight:'20px'}} to={'/who'}>Login <FiLogIn style={{marginBottom:'5px',marginLeft:'2px'}}/></NavLink>
             </ul>
-            <FacultySignIn/>
         </>
     )
 }
