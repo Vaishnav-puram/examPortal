b14de47d57af16e1b3e3f9493225585aa640bbe3
Vaishnav Puram <puramv307@gmail.com>
2024-02-19 23:27:03 +0530

    git commit -m “feat: custom query to find max score by grouping quizzes"

--- src/main/java/com/exam/examportal/repo/ResultRepo.java
+++ src/main/java/com/exam/examportal/repo/ResultRepo.java
@@ -1,7 +1,10 @@
 package com.exam.examportal.repo;
 
+import com.exam.examportal.dto.ResultMaxDTO;
 import com.exam.examportal.dto.ResultResponseDTO;
+import jakarta.persistence.Tuple;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +12,7 @@ import java.util.List;
 @Repository
 public interface ResultRepo extends JpaRepository<ResultResponseDTO,Long> {
     List<ResultResponseDTO> findByRollno(String rollno);
+
+    @Query(value = "SELECT r.quiz,MAX(r.result) AS result FROM results r where r.rollno=:rollno GROUP BY r.quiz",nativeQuery = true)
+    List<Tuple> getData(String rollno);
 }
