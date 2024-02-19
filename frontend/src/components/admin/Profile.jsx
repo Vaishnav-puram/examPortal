d569c094193e704897d8500abaab645f75e90050
Vaishnav Puram <puramv307@gmail.com>
2024-02-19 20:06:14 +0530

    git commit -m “feat: displaying DP"

--- frontend/src/components/admin/Profile.jsx
+++ frontend/src/components/admin/Profile.jsx
@@ -1,29 +1,61 @@
-import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
-import "../../index.css"
+import { useEffect, useState } from "react";
+import { getImageFromRollno } from "../../services/User_Service";
+import "./adminProfile.css"
 function Profile() {
+    let [imageData, setImageData] = useState(null);
+
+    let id = JSON.parse(localStorage.getItem('user')).rollno;
+    useEffect(() => {
+        const fetchImage = async () => {
+            try {
+                const res = await getImageFromRollno(id);
+                console.log("response -->", res.data);
+                setImageData(res.data);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        if (id) {
+            fetchImage();
+        }
+    }, [id])
+    console.log("url -->",`data:image/jpeg;base64,${imageData}`);
     return (
         <>
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-                <Card.Body>
-                    <Card.Title>{localStorage.getItem('name')}</Card.Title>
-                    <Card.Text>
-                        {localStorage.getItem('role')}
-                    </Card.Text>
-                </Card.Body>
-                <ListGroup className="list-group-flush">
-                    <ListGroup.Item>User ID : <b>{JSON.parse(localStorage.getItem('user')).rollno}</b></ListGroup.Item>
-                    <ListGroup.Item>First Name : <b>{JSON.parse(localStorage.getItem('currUser')).firstname}</b></ListGroup.Item>
-                    <ListGroup.Item>Last Name : <b>{JSON.parse(localStorage.getItem('currUser')).lastname}</b></ListGroup.Item>
-                    <ListGroup.Item>Email : <b>{JSON.parse(localStorage.getItem('currUser')).email}</b></ListGroup.Item>
-                    <ListGroup.Item>Age : <b>{JSON.parse(localStorage.getItem('currUser')).age}</b></ListGroup.Item>
-                    <ListGroup.Item>Mobile : <b>{JSON.parse(localStorage.getItem('currUser')).mobile}</b></ListGroup.Item>
-                </ListGroup>
-                <Card.Body>
-                    <Card.Link href="#">Edit</Card.Link>
-                </Card.Body>
-            </Card>
+            <div className="container mt-4 mb-4 p-3 d-flex justify-content-center">
+                <div className="card1">
+                    <div className="image d-flex flex-column justify-content-center align-items-center">
+                        <img src={`data:image/jpeg;base64,${imageData}`} height={'200px'} /><span className="name mt-3">{JSON.parse(localStorage.getItem('currUser')).firstname}</span> <span className="idd">{JSON.parse(localStorage.getItem('currUser')).lastname}</span>
+                        <div className="d-flex flex-row justify-content-center align-items-center gap-2">
+                            <span className="idd1"><b>{localStorage.getItem('role')} </b></span>
+                        </div>
+                        <div className="d-flex flex-row justify-content-center align-items-center mt-3">
+                            <span className="number">{JSON.parse(localStorage.getItem('user')).rollno}</span>
+                        </div>
+                        <div style={{ fontSize: 'medium' }}>
+                            <table>
+                                <tr>
+                                    <td>Mail : <b>{JSON.parse(localStorage.getItem('currUser')).email}</b></td>
+
+                                </tr>
+                                <tr>
+                                    <td>Age : <b>{JSON.parse(localStorage.getItem('currUser')).age}</b></td>
+
+                                </tr>
+                                <tr>
+                                    <td>Mobile : <b>{JSON.parse(localStorage.getItem('currUser')).mobile}</b></td>
+
+                                </tr>
+
+                            </table>
+                        </div>
+                        <div className="d-flex mt-2"><button className="btn1 btn-dark">Edit Profile</button></div>
+                        <div className="gap-3 mt-3 icons d-flex flex-row justify-content-center align-items-center">
+                            <span><i className="fa fa-twitter"></i></span> <span><i className="fa fa-facebook-f"></i></span> <span><i className="fa fa-instagram"></i></span> <span><i className="fa fa-linkedin"></i></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
