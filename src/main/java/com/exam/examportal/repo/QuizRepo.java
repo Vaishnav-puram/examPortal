262f22d2cbe11e18b5bb7ce756009159ba79c2a4
Vaishnav Puram <puramv307@gmail.com>
2024-02-19 23:26:07 +0530

    git commit -m “feat: find quiz by title"

--- src/main/java/com/exam/examportal/repo/QuizRepo.java
+++ src/main/java/com/exam/examportal/repo/QuizRepo.java
@@ -9,7 +9,7 @@ import java.util.Set;
 
 public interface QuizRepo extends JpaRepository<Quiz,Long> {
     Quiz findByQid(Long qid);
-
+    Quiz findByTitle(String title);
     @Query("select q from Quiz q where q.category is not null")
     Set<Quiz> findAllNonNullValues();
     @Query("SELECT q FROM Quiz q JOIN q.category c WHERE c.title = :subject")
