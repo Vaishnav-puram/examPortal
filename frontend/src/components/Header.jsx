9b96fe9f3888924d3af56f590ac0e442d48708e9
Vaishnav Puram <puramv307@gmail.com>
2024-02-19 20:12:16 +0530

    git commit -m “chore:refactored"

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,26 +1,34 @@
 import IACSDLogo from "../images/IACSD_Logo.png";
 import CDACLogo from "../images/cdacActs.png";
-function Header(){
-    let st={
-        marginTop: '30px',
+function Header() {
+    let st = {
+        paddingTop: '5px',
         display: 'flex',
         justifyContent: 'space-between',
-        alignItems: 'center'
+        alignItems: 'center',
+        height: '90px'
     }
+    let titleStyle = {
+        textAlign: 'center',
+        color: '#227396',
+        fontSize: '28px'
+    };
+    let logoStyle = {
+        width: '100px',
+        height: 'auto'
+    };
 
-    return(
-        <>
-        <div style={st}>
-        <img src={IACSDLogo} />
-        <div>
-       
-        <h3 style={{display:'flex',justifyContent:'center',color:'#227396',fontSize:'30px'}}>Dr. D. Y. Patil Pratishthan's</h3>
-        <h3 style={{display:'flex',justifyContent:'center',color:'#227396',fontSize:'30px'}}>Institute for Advanced Computing and Software Development</h3>
-       
+    return (
+        <div style={{ backgroundColor: 'white' }}>
+            <div style={st}>
+                <img src={IACSDLogo} style={logoStyle} />
+                <div>
+                    <h3 style={titleStyle}>Dr. D. Y. Patil Pratishthan's</h3>
+                    <h3 style={titleStyle}>Institute for Advanced Computing and Software Development</h3>
+                </div>
+                <img src={CDACLogo} style={{ width: '200px' ,logoStyle}} />
+            </div><br /><br /><br />
         </div>
-        <img src={CDACLogo} style={{width:'200px'}} />
-        </div><br/><br/><br/>
-        </>
     )
 
 }
