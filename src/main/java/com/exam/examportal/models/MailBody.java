bb2b09009d8d015b27b126f65d9480ff13137108
Vaishnav Puram <puramv307@gmail.com>
2024-02-19 23:31:23 +0530

    git commit -m “feat:mail body"

---
+++ src/main/java/com/exam/examportal/models/MailBody.java
@@ -0,0 +1,25 @@
+package com.exam.examportal.models;
+
+public class MailBody {
+    private String to;
+    private String body;
+
+    public MailBody() {
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
