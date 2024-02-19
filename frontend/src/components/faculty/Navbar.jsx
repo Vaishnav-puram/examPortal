6454e6279c03d5b8f38d2fb519d0367ef2497392
Vaishnav Puram <puramv307@gmail.com>
2024-02-19 20:08:06 +0530

    git commit -m “chore: refactored"

--- frontend/src/components/faculty/Navbar.jsx
+++ frontend/src/components/faculty/Navbar.jsx
@@ -1,24 +1,28 @@
-import { NavLink,useNavigate} from "react-router-dom";
-import {logoff,currFacultyName} from "../../services/User_Service";
-import { IoHome } from "react-icons/io5";
-import { GrLogout } from "react-icons/gr";
+import { NavLink} from "react-router-dom";
+import { getRole,logoff,getUserName} from "../../services/User_Service";
+import { useState } from "react";
+import Sidebar from "./Sidebar";
 import "../../index.css"
 function Navbar(){
-    const navigate=useNavigate();
-    const navigateToHome=()=>{
-        navigate('/faculty-dashboard')
+    const [isSidebarOpen,setIsSidebarOpen]=useState(false);
+    const toogleSidebar=()=>{
+        setIsSidebarOpen(!isSidebarOpen);
     }
+    let role=getRole();
     const handleLogout=()=>{
         logoff(); //clears the token, userdetails from local storage
     }
-    let fname=currFacultyName();
+     let uname=getUserName();
+    console.log(role);
     return(
         <>
              <ul>
-                <span id="menu" style={{float:'left', marginRight:'30px'}} onClick={navigateToHome}><IoHome style={{marginBottom:'5px',marginRight:'2px'}}/>Home</span>
-                <span id="registered" style={{float:'right', marginRight:'30px'}} to={'/signup'}>{fname.toUpperCase()}</span>
-                <NavLink id="loginBtn" style={{float:'right', marginRight:'20px'}} to={'/'} onClick={handleLogout}>Logout <GrLogout style={{marginBottom:'5px',marginRight:'2px'}}/></NavLink>   
+                <span id="menu" style={{float:'left', marginRight:'30px'}} onClick={toogleSidebar}>Menu</span>
+                {/* <NavLink to={'/admin-dashboard/menu'} id="menu">Menu</NavLink> */}
+                <span id="registered" style={{float:'right', marginRight:'30px'}} to={'/signup'}>{uname}</span>
+                <NavLink id="loginBtn" style={{float:'right', marginRight:'20px'}} to={'/'} onClick={handleLogout}>Logout</NavLink>   
             </ul>
+            {isSidebarOpen && <Sidebar/>}
         </>
     )
 }
