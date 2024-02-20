6c4c794c8358ba169cbe11631d6d667d403611ef
Vaishnav Puram <puramv307@gmail.com>
2024-02-20 22:26:35 +0530

    git commit -m “feat:sidebar update"

--- frontend/src/components/faculty/Sidebar.jsx
+++ frontend/src/components/faculty/Sidebar.jsx
@@ -8,6 +8,7 @@ import { NavLink} from "react-router-dom";
 import { useState,useEffect } from 'react';
 import { getCategories } from '../../services/User_Service';
 import { IoIosSend } from "react-icons/io";
+import { IoBookSharp } from "react-icons/io5";
 function Sidebar() {
     let [categories,setCategories]=useState([]);
     useEffect(()=>{
@@ -29,17 +30,16 @@ function Sidebar() {
                 <CDBSidebar maxWidth='345px' textColor="#fff" backgroundColor="#333">
                     <CDBSidebarContent>
                         <CDBSidebarMenu>
-                            <NavLink to={'/faculty-dashboard'} activeClassName="activeClicked">
+                            <NavLink to={'/faculty-dashboard/quizzes'} activeClassName="activeClicked">
                                 <CDBSidebarMenuItem icon="home">Home</CDBSidebarMenuItem>
                             </NavLink>
                             <NavLink to={'/faculty-dashboard/mail'} activeClassName="activeClicked">
                                 <CDBSidebarMenuItem><IoIosSend/> Notify Students</CDBSidebarMenuItem>
                             </NavLink>
-                            {categories.map((category)=>(
-                                <NavLink to={`/user-dashboard/quizzes/${category.title}`} key={category.cid} activeClassName="activeClicked">
-                                <CDBSidebarMenuItem>{category.title}</CDBSidebarMenuItem>
-                                </NavLink>
-                            ))}
+                            <NavLink to={'/faculty-dashboard/quizComponentView'} activeClassName="activeClicked">
+                                <CDBSidebarMenuItem><IoBookSharp /> View Questions</CDBSidebarMenuItem>
+                            </NavLink>
+                            
                             
                         </CDBSidebarMenu>
                         
