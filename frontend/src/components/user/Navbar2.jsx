bbf830a3ba413c5a981852dc88be62013dba1972
Vaishnav Puram <puramv307@gmail.com>
2024-02-20 11:35:01 +0530

    git commit -m “feat:added timer"

--- frontend/src/components/user/Navbar2.jsx
+++ frontend/src/components/user/Navbar2.jsx
@@ -1,10 +1,16 @@
-import { NavLink} from "react-router-dom";
-import { getRole,logoff,getUserName,getRollno} from "../../services/User_Service";
-import { useState,useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import { getRole, logoff, getUserName, getRollno } from "../../services/User_Service";
+import { useState, useEffect } from "react";
 import { getImageFromRollno } from "../../services/User_Service";
 import "../../index.css"
-function Navbar2(){
+import "./user.css"
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+function Navbar2() {
     let [imageData, setImageData] = useState(null);
+    const totalTimeInMinutes=20;
+    let totalTimeInSeconds=totalTimeInMinutes*60;
+    let [remainingTime,setRemainingTime]=useState(totalTimeInSeconds);
 
     let id = JSON.parse(localStorage.getItem('user')).rollno;
     useEffect(() => {
@@ -21,19 +27,41 @@ function Navbar2(){
             fetchImage();
         }
     }, [id])
-    let role=getRole();
-    const handleLogout=()=>{
+
+    useEffect(()=>{
+        const timer=setInterval(()=>{
+            setRemainingTime(prev=>prev-1)
+            if(remainingTime==0){
+                clearInterval(timer);
+            }
+        },1000)
+        return ()=>clearInterval(timer);
+    },[remainingTime])
+
+    const minutes=Math.floor(remainingTime/60);
+    const progressPercentage = (totalTimeInSeconds - remainingTime) / totalTimeInSeconds * 100;
+    let role = getRole();
+    const handleLogout = () => {
         logoff(); //clears the token, userdetails from local storage
     }
-    let uname=getUserName();
-    let rollno=getRollno();
+    let uname = getUserName();
+    let rollno = getRollno();
     console.log(role);
-    return(
+    return (
         <>
-            <img style={{width:'fit-content'}} src={`data:image/jpeg;base64,${imageData}`} height={'200px'}/>
-             <ul>
-                <span id="registered" style={{float:'right', marginRight:'30px'}} to={'/signup'}>{uname} ({rollno}) </span>
-                <NavLink id="loginBtn" style={{float:'right', marginRight:'20px'}} to={'/'} onClick={handleLogout}>Logout</NavLink>   
+            <img style={{ width: 'fit-content' }} src={`data:image/jpeg;base64,${imageData}`} height={'200px'} />
+            <div style={{ position:'absolute',right:'25px',top:'22%',width: 140, height: 100 }}>
+                <CircularProgressbar value={progressPercentage} text={`${minutes}m`}  counterClockwise={true} 
+                    styles={buildStyles({
+                        textColor:'#fff',
+                        pathColor:'red',
+
+                    })}
+                />
+            </div>
+            <ul>
+                <span id="registered" style={{ float: 'right', marginRight: '30px' }} to={'/signup'}>{uname} ({rollno}) </span>
+                <NavLink id="loginBtn" style={{ float: 'right', marginRight: '20px' }} to={'/'} onClick={handleLogout}>Logout</NavLink>
             </ul>
         </>
     )
--- frontend/src/components/user/QuestionPaper.jsx
+++ frontend/src/components/user/QuestionPaper.jsx
@@ -1,17 +1,21 @@
 import { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import { useParams,useNavigate } from 'react-router-dom';
-import { getImageFromQuestion, getQuestions, getResults } from '../../services/User_Service';
+import { useParams, useNavigate } from 'react-router-dom';
+import { getQuestions, getResults } from '../../services/User_Service';
 import Form from 'react-bootstrap/Form';
 import "./user.css"
 import Header from '../Header';
 import Navbar2 from './Navbar2';
 function QuestionPaper() {
     const { qid } = useParams();
-    const navigate=useNavigate();
+    const navigate = useNavigate();
     let id = JSON.parse(localStorage.getItem('user')).rollno;
     let [questionData, setQuestionData] = useState([]);
+    let [timerCompleted, setTimerCompleted] = useState(false);
+    const totalTime = 20;
+    const totalTimeInSec = totalTime * 60;
+    let [remainingTime, setRemainingTime] = useState(totalTimeInSec);
     useEffect(() => {
         fetchQuestions();
     }, [])
@@ -19,42 +23,55 @@ function QuestionPaper() {
         try {
             const res = await getQuestions(qid);
             console.log("questions-->", res.data);
-            res.data.forEach(async(question)=>{
-                question["givenAnswer"]=""
+            res.data.forEach(async (question) => {
+                question["givenAnswer"] = ""
             })
             setQuestionData(res.data);
         } catch (err) {
             console.log(err);
         }
     }
-    console.log("new questions--> " ,questionData);
+    
+    useEffect(()=>{
+        const timer=setInterval(()=>{
+            setRemainingTime(prev=>prev-1)
+            console.log("remaining time--> ",remainingTime);
+            if(remainingTime==0){
+                setTimerCompleted(true);
+                clearInterval(timer);
+                handleResults();
+            }
+        },1000)
+        return ()=>clearInterval(timer);
+    },[remainingTime])
+    console.log("new questions--> ", questionData);
 
     const handleSelected = (queId, selectedOption) => {
-        console.log("selected question --> ",queId);
-        console.log("selected option-->",selectedOption);
-        const updatedQuestions=questionData.map((question)=>{
-            if(question.queId===queId){
-                console.log("matched-->",queId);
-                return{
+        console.log("selected question --> ", queId);
+        console.log("selected option-->", selectedOption);
+        const updatedQuestions = questionData.map((question) => {
+            if (question.queId === queId) {
+                console.log("matched-->", queId);
+                return {
                     ...question,
-                    givenAnswer:selectedOption
+                    givenAnswer: selectedOption
                 };
             }
             return question;
         });
         setQuestionData(updatedQuestions);
     };
-    const handleIndexColor=(index1)=>{
-        console.log("inside handle color --> ",index1);
-        document.getElementsByClassName(index1)[0].style.backgroundColor='green';
+    const handleIndexColor = (index1) => {
+        console.log("inside handle color --> ", index1);
+        document.getElementsByClassName(index1)[0].style.backgroundColor = 'green';
     }
-    const handleResults=async (e)=>{
-        console.log("inside questionpaper --> ",questionData);
+    const handleResults = async (e) => {
+        console.log("inside questionpaper --> ", questionData);
         e.preventDefault();
-        try{
-            const result=await getResults(questionData,id);
+        try {
+            const result = await getResults(questionData, id);
             console.log(result.data);
-        }catch(err){
+        } catch (err) {
             console.log(err);
         }
         navigate("/user-dashboard/results");
@@ -66,28 +83,28 @@ function QuestionPaper() {
             <div className='gridContainer'>
                 <div className='item-1'>
                     <div className='innerGridComponent'>
-                        {questionData.map((question,index1)=>{
-                            console.log("inside index --> ",index1);
-                            var idx=index1+1;
-                        return (
-                            <a className={idx} style={{textDecoration:'none',color:'black'}} href={`#${index1}`} key={question.queId}><div className='box' >{++index1}</div></a>
-                        );
+                        {questionData.map((question, index1) => {
+                            console.log("inside index --> ", index1);
+                            var idx = index1 + 1;
+                            return (
+                                <a className={idx} style={{ textDecoration: 'none', color: 'black' }} href={`#${index1}`} key={question.queId}><div className='box' >{++index1}</div></a>
+                            );
                         })}
                     </div>
                 </div>
                 <div className='item-2'>
-                    {questionData.map((question,index) => (
+                    {questionData.map((question, index) => (
                         <div className='question' key={question.queId} style={{ marginBottom: '30px', boxShadow: ' 10px 10px 15px grey' }}>
                             <Card>
                                 <Card.Header id={index}><b>{++index} ) </b>{question.content}</Card.Header>
                                 <Card.Body>
-                                {question.image &&(<img src={`data:image/jpeg;base64,${question.image}`} />)}
+                                    {question.image && (<img src={`data:image/jpeg;base64,${question.image}`} />)}
                                     <Card.Text>
                                         <Form className='options' style={{ fontWeight: 'normal' }}>
-                                            <div className='parent_OPT1'><Form.Check className='opt1' type='radio' name='opt' value={question.option_1} id='option_1' label={question.option_1} onChange={()=>{handleSelected(question.queId,question.option_1);handleIndexColor(index)}}/></div>
-                                            <div className='parent_OPT2'><Form.Check className='opt2' type='radio' name='opt' value={question.option_2} id='option_2' label={question.option_2} onChange={()=>{handleSelected(question.queId,question.option_2);handleIndexColor(index)}}/></div>
-                                            <div className='parent_OPT3'><Form.Check className='opt3' type='radio' name='opt' value={question.option_3} id='option_3' label={question.option_3} onChange={()=>{handleSelected(question.queId,question.option_3);handleIndexColor(index)}}/></div>
-                                            <div className='parent_OPT4'><Form.Check className='opt4' type='radio' name='opt' value={question.option_4} id='option_4' label={question.option_4} onChange={()=>{handleSelected(question.queId,question.option_4);handleIndexColor(index)}}/></div>
+                                            <div className='parent_OPT1'><Form.Check className='opt1' type='radio' name='opt' value={question.option_1} id='option_1' label={question.option_1} onChange={() => { handleSelected(question.queId, question.option_1); handleIndexColor(index) }} /></div>
+                                            <div className='parent_OPT2'><Form.Check className='opt2' type='radio' name='opt' value={question.option_2} id='option_2' label={question.option_2} onChange={() => { handleSelected(question.queId, question.option_2); handleIndexColor(index) }} /></div>
+                                            <div className='parent_OPT3'><Form.Check className='opt3' type='radio' name='opt' value={question.option_3} id='option_3' label={question.option_3} onChange={() => { handleSelected(question.queId, question.option_3); handleIndexColor(index) }} /></div>
+                                            <div className='parent_OPT4'><Form.Check className='opt4' type='radio' name='opt' value={question.option_4} id='option_4' label={question.option_4} onChange={() => { handleSelected(question.queId, question.option_4); handleIndexColor(index) }} /></div>
                                         </Form>
                                         <Button className='clearBtn'>Clear answer</Button>
                                     </Card.Text>
@@ -96,7 +113,7 @@ function QuestionPaper() {
                         </div>
                     ))}
                     <div>
-                <Button className='questionSubmit' variant="outline-success" onClick={handleResults}>Submit</Button>
+                            <Button className='questionSubmit' variant="outline-success" onClick={handleResults} disabled={timerCompleted}>Submit</Button>
                     </div>
                 </div>
             </div>
