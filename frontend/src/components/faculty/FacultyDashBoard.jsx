ef7d88d8539ea6800d26499cf21319387ed84a1b
Vaishnav Puram <puramv307@gmail.com>
2024-02-20 22:25:54 +0530

    git commit -m “chore:refactored"

--- frontend/src/components/faculty/FacultyDashBoard.jsx
+++ frontend/src/components/faculty/FacultyDashBoard.jsx
@@ -1,7 +1,7 @@
+import { Outlet } from "react-router-dom";
 import { currSubject } from "../../services/User_Service";
 import Header from "../Header";
 import Navbar from "./Navbar";
-import QuizComponent from "./QuizComponent";
 function FacultyDashBoard(){
     var subject=currSubject();
     return(
@@ -10,7 +10,7 @@ function FacultyDashBoard(){
             <Navbar/>
             <h3 style={{color:'white'}}>Welcome to Faculty page</h3>
             <h2 style={{color:'white'}}>Category :{subject} </h2>
-            <QuizComponent/>
+            <Outlet/>
         </>
     )
 }
